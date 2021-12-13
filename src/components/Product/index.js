6529cccbba48ea0c255540541b572dca776a599d
ben2613 <sosgyuki@gmail.com>
2021-12-13 13:55:31 +0000

    functional

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import { Dimensions, ImageBackground, StyleSheet, View, Text } from "react-native";
 
-import { ImageBackground, StyleSheet, View } from "react-native";
+// Get window box for Flat list height
+const { height, width } = Dimensions.get('window');
 
-const Product = ({data}) => {
+const calculatePrice = (data) => {
+  let ret = 0;
+  if (data.discount_type === 'percentage') {
+    ret = Math.round(data.price * (100 - data.discount) / 100);
+  } else if (data.discount_type === 'amount') {
+    ret = data.price - data.discount;
+  } else {
+    ret = data.price;
+  }
+  return formatPrice(ret)
+};
+const formatPrice = (numPrice) => {
+  if (numPrice === 0) {
+    return 'Free'
+  }
+  const s = numPrice.toString();
+  return '\u00A3' + s.substring(0,s.length-2) + '.' + s.substring(s.length-2)
+};
+const Product = ({ data }) => {
   return (
     <View style={styles.container}>
-      <ImageBackground source={{ uri: data.image }} resizeMode="cover" style={styles.image}>
-
+      <ImageBackground source={{ uri: data.image }} resizeMode="cover" style={styles.imageView}>
+        <View style={styles.info}>
+          <Text style={styles.titlePrice}>{data.name}</Text>
+          <Text style={styles.titlePrice}>
+            {calculatePrice(data)}
+          </Text>
+          <Text style={styles.short_desc}>{data.short_description}</Text>
+        </View>
       </ImageBackground>
     </View>
   );
@@ -15,21 +41,27 @@ const Product = ({data}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    height: 0.3 * height,
   },
-  image: {
-    flex: 1,
-    justifyContent: "center",
-    aspectRatio: 4/3,
-    height: 100,
+  imageView: {
+    // flex: 1,
+    justifyContent: "flex-end",
+    aspectRatio: 4 / 3,
+    height: '100%',
     width: null,
+    borderRadius: 15,
+    overflow: 'hidden',
   },
-  text: {
-    color: "white",
-    fontSize: 42,
-    lineHeight: 84,
-    fontWeight: "bold",
-    textAlign: "center",
-    backgroundColor: "#000000c0"
+  info: {
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    padding: 6,
+  },
+  titlePrice: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  short_desc: {
+    color: 'white',
   }
 });
 
